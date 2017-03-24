e70682298ba3848c95852f663de879528109e054
Thejas Naik <naiktheju@gmail.com>
2017-03-25 03:41:13 +0530

    Main.c
    Program cured of a couple of errors :D

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <GL/glut.h>
 #include <stdlib.h>
 #include <stdio.h>
+//#define point3 int
 
 GLfloat xRotated, yRotated, zRotated;
 GLdouble radius=1;
+
 
 
 void display(void);
@@ -24,53 +26,67 @@ void myinit()
 
 }
 
+
+
+void horizontal()
+{
+float M_PI = 3.14159;
 const float DegreesToRadians = M_PI / 180.0; // M_PI = 3.14159...
-point3 quad_data[342];
+//point3
+int quad_data[342];
 // 8 rows of 18 quads
 int k = 0;
-for(float phi = -80.0; phi <= 80.0; phi += 20.0)
+float phi,theta;
+for(phi = -80.0; phi <= 80.0; phi += 20.0)
 {
 float phir
 = phi*DegreesToRadians;
 float phir20 = (phi + 20.0)*DegreesToRadians;
-for(float theta = -180.0; theta <= 180.0; theta += 20.0)
+for(theta = -180.0; theta <= 180.0; theta += 20.0)
 {
 float thetar = theta*DegreesToRadians;
-quad_data[k] = point3(sin(thetar)*cos(phir),
-cos(thetar)*cos(phir), sin(phir));
+quad_data[k] = (int)(sin(thetar)*cos(phir),cos(thetar)*cos(phir), sin(phir));
 k++;
-quad_data[k] = point3(sin(thetar)*cos(phir20),
-cos(thetar)*cos(phir20), sin(phir20));
+quad_data[k] = (int)(sin(thetar)*cos(phir20),cos(thetar)*cos(phir20), sin(phir20));
+k++;
+}
+}
+}
+
+void vertical()
+{
+float M_PI = 3.14159;
+const float DegreesToRadians = M_PI / 180.0; // M_PI = 3.14159...
+int k = 0;
+//point3
+int strip_data[40];
+strip_data[k] = (int)(0.0, 0.0, 1.0);
+k++;
+float sin80 = sin(80.0*DegreesToRadians);
+float cos80 = cos(80.0*DegreesToRadians);
+float theta;
+for(theta = -180.0; theta <= 180.0; theta += 20.0)
+{
+float thetar = theta*DegreesToRadians;
+strip_data[k] = (int)(sin(thetar)*cos80,cos(thetar)*cos80, sin80);
+k++;
+}
+strip_data[k] = (int)(0.0, 0.0, -1.0);
+k++;
+for(theta = -180.0; theta <= 180.0; theta += 20.0)
+{
+float thetar = theta;
+strip_data[k] = (int)(sin(thetar)*cos80,cos(thetar)*cos80, sin80);
 k++;
 }
 }
 
-const float DegreesToRadians = M_PI / 180.0; // M_PI = 3.14159...
-int k = 0;
-point3 strip_data[40];
-strip_data[k] = point3(0.0, 0.0, 1.0);
-k++;
-float sin80 = sin(80.0*DegreesToRadians);
-float cos80 = cos(80.0*DegreesToRadians);
-6162
-Chapter 2
-Graphics Programming
-for(float theta = -180.0; theta <= 180.0; theta += 20.0)
+void display()
 {
-float thetar = theta*DegreesToRadians;
-strip_data[k] = point3(sin(thetar)*cos80,
-cos(thetar)*cos80, sin80);
-k++;
+  horizontal();
+  vertical();
 }
-strip_data[k] = point3(0.0, 0.0, -1.0);
-k++;
-for(float theta = -180.0; theta <= 180.0; theta += 20.0)
-{
-float thetar = theta;
-strip_data[k] = point3(sin(thetar)*cos80,
-cos(thetar)*cos80, sin80);
-k++;
-}
+
 
 int main (int argc, char **argv)
 {
@@ -82,7 +98,7 @@ int main (int argc, char **argv)
     yRotated=50;
 
     glutDisplayFunc(display);
-    glutReshapeFunc(reshape);
+//    glutReshapeFunc(reshape);
     glutMainLoop();
     return 0;
 }
